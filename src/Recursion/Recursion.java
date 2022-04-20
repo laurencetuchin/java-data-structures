08e0ec37514b6949101ef1a39a8e7a11874037a6
laurencetuchin <laurencetuchin@gmail.com>
2022-04-20 11:42:03 +1000

    recursion power

--- src/Recursion/Recursion.java
+++ src/Recursion/Recursion.java
@@ -2,12 +2,13 @@ package Recursion;
 
 public class Recursion {
     public static void main(String[] args) {
-        System.out.println(factorial(7));
+        System.out.println(power(2,8));
+
     }
 
-    private static int factorial(int num) {
-        if(num < 1) return 1;
-        return num * factorial(num -1);
+    private static int power(int base, int exponent) {
+        if(exponent < 1) return 1;
+        return base * power(base, exponent - 1);
     }
 
 
