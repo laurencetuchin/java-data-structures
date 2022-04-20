d0c63b0d53b0e752022b608db755935c95a188f1
laurencetuchin <laurencetuchin@gmail.com>
2022-04-20 20:11:34 +1000

    hashtable collision

--- src/HashTable/HashTable.java
+++ src/HashTable/HashTable.java
@@ -3,19 +3,19 @@ import java.util.*;
 
 public class HashTable {
     public static void main(String[] args) {
-        Hashtable<Integer, String> table = new Hashtable<>(10);
+        Hashtable<String, String> table = new Hashtable<>(21);
 
-        table.put(100, "Spongebob");
-        table.put(123, "Patrick");
-        table.put(321, "Sandy");
-        table.put(555, "Squidward");
-        table.put(777, "Gary");
+        table.put("100", "Spongebob");
+        table.put("123", "Patrick");
+        table.put("321", "Sandy");
+        table.put("555", "Squidward");
+        table.put("777", "Gary");
 
 //        table.remove(777);
 
-        for(Integer key : table.keySet()) {
+        for(String key : table.keySet()) {
 
-        System.out.println(key.hashCode() % 10 + "\t" + key + "\t" + table.get(key));
+        System.out.println(key.hashCode() % 21 + "\t" + key + "\t" + table.get(key));
         }
     }
 }
