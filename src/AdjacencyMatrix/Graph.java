843f41091b64a80da6cc57fd3d39fa6420b9455a
laurencetuchin <laurencetuchin@gmail.com>
2022-04-21 12:00:09 +1000

    adjacency matrix graph

---
+++ src/AdjacencyMatrix/Graph.java
@@ -0,0 +1,41 @@
+package AdjacencyMatrix;
+
+import java.util.ArrayList;
+
+public class Graph {
+    ArrayList<Node> nodes;
+    int[][] matrix;
+
+    Graph(int size) {
+        nodes = new ArrayList<>();
+        matrix = new int[size][size];
+    }
+
+    public void addNode(Node node) {
+        nodes.add(node);
+    }
+
+    public void addEdge(int src, int dst) {
+        matrix[src][dst] = 1;
+    }
+
+    public boolean checkEdge(int src, int dst) {
+        if(matrix[src][dst] == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void print() {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
