d10aca168580a86102d23ef9c46dae398189d49b
laurencetuchin <laurencetuchin@gmail.com>
2022-04-25 22:19:15 +1000

    Library HashMap

---
+++ src/Library/Library.java
@@ -0,0 +1,10 @@
+package Library;
+
+import java.util.HashMap;
+
+public class Library {
+    HashMap<Book, String> borrowers = new HashMap<>();
+
+    Book bookObject = new Book("Lord of the rings","Destroy the ring",2001);
+
+}
